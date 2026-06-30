bfaf375cf5d7c058cf2b80b350fd89aa368cf59c
Indu Bala <indubala@gmail.com>
2026-06-30 22:48:12 +0530

    Added new changes in test case

--- UserControllerShould/UserControllerShould.cs
+++ UserControllerShould/UserControllerShould.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using Order.API.Controllers;
 using Order.Data.Interfaces;
+using Order.Dto.DTO.Request;
 using Order.Dto.DTO.Response;
 
 namespace UserControllerShould
@@ -59,5 +60,136 @@ namespace UserControllerShould
             data.Should().HaveCount(2);
             
         }
+
+
+        [Fact]
+
+        public async Task GivenRequest_WhenGetById_ThenRetunNotFound()
+        {
+            //Arrange
+            _userMock.GetById(1).Returns((UserResponse)null);   
+            //Act
+            var result= await _userController.Getbyid(1);
+            //Assert
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+
+        [Fact]
+
+        public async Task GivenRequest_WhenGetById_ThenRetunUser()
+        {
+            //Arrange
+
+            var user = new UserResponse
+            {
+                Id = 1,
+                Name = "Ram"
+            };
+
+
+
+            _userMock.GetById(1).Returns(user);
+            //Act
+            var result = await _userController.Getbyid(1);
+            //Assert
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+
+        #region Create TEst user
+
+        [Fact]
+        public async Task GivenRequest_WhenCreate_ThenReturnCreatedUser()
+        {
+            //Arrange
+
+            var userRequest = new UserRequest
+            {
+                Name = "Ram"
+            };
+
+
+            var userResponse = new UserResponse
+            {
+                Id = 1,
+                Name = "Ram"
+            };
+
+            _userMock.Create(userRequest).Returns(userResponse);
+
+
+            //Act
+            var result= await _userController.Create(userRequest);
+
+
+
+            ///Assert
+            ///
+            result.Should().BeOfType<OkObjectResult>();
+
+
+        }
+
+        #endregion
+
+        #region Upate user
+
+        [Fact]
+        public async Task GivenRequest_WhenUpdate_ThenReturnUpdatedUser()
+        {
+            //Arrange
+
+            var userRequest = new UserRequest
+            {
+                Name = "Ram updated"
+            };
+
+
+            var userResponse = new UserResponse
+            {
+                Id = 1,
+                Name = "Ram updated"
+            };
+
+            _userMock.Update(1,userRequest).Returns(userResponse);
+
+
+            //Act
+            var result = await _userController.Update(1,userRequest);
+
+
+
+            ///Assert
+            ///
+            result.Should().BeOfType<OkObjectResult>();
+
+
+        }
+
+        #endregion
+
+        [Fact]
+
+        public async Task GivenValidRequest_WhenDelete_ThenReturnTrue()
+        {
+
+            ///Arrange
+            _userMock.Delete(1).Returns(true);
+
+
+            //Act
+
+            var result= await _userController.Delete(1);
+
+            //Asset
+            result.Should().BeOfType<OkObjectResult>();
+
+
+        }
+
+
     }
 }
